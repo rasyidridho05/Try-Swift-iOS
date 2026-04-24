3553af9f38e827472866b8eaed0f38412134b12b
rasyidridho05 <rasyid.rdh05@gmail.com>
2026-04-24 14:55:48 +0700

    Initial Commit

---
+++ iOS APP/iOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_APPApp.swift
+//  iOS APP
+//
+//  Created by Muhammad Rasyid Ridho on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
